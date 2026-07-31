c6171bb5c43f5d5647b10ce034fe3d480f6f5a77
TimofeyHarkunov <tharkunov@gmail.com>
2026-07-31 10:04:02 +0300

    1. Чтение кода и базовые ошибки [#40205] - Исправил ошибку для сборки;

--- src/test/java/ru/job4j/loop/FitnessTest.java
+++ src/test/java/ru/job4j/loop/FitnessTest.java
@@ -9,7 +9,7 @@ class FitnessTest {
     void whenIvanGreatNik() {
         int ivan = 95;
         int nik = 90;
-        int result = Fitness.calc(ivan, nik);
+        int result = Fitness.calculate(ivan, nik);
         int expected = 0;
         assertThat(result).isEqualTo(expected);
     }
@@ -18,7 +18,7 @@ class FitnessTest {
     void whenIvanLessByOneNik() {
         int ivan = 90;
         int nik = 95;
-        int result = Fitness.calc(ivan, nik);
+        int result = Fitness.calculate(ivan, nik);
         int expected = 1;
         assertThat(result).isEqualTo(expected);
     }
@@ -27,7 +27,7 @@ class FitnessTest {
     void whenIvanLessByFewNik() {
         int ivan = 50;
         int nik = 90;
-        int result = Fitness.calc(ivan, nik);
+        int result = Fitness.calculate(ivan, nik);
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
@@ -36,7 +36,7 @@ class FitnessTest {
     void whenIvanEqualsNik() {
         int ivan = 90;
         int nik = 90;
-        int result = Fitness.calc(ivan, nik);
+        int result = Fitness.calculate(ivan, nik);
         int expected = 1;
         assertThat(result).isEqualTo(expected);
     }
